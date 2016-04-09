ea1a74c5a09be26048c570001c61efc8d6cee587
Tim Meijome <tim.meijome@gmail.com>
2016-04-09 10:24:01 -0600

    update

---
+++ program_5.js
@@ -0,0 +1,14 @@
+var mongo = require('mongodb').MongoClient
+var url = 'mongodb://localhost:27017/' + process.argv[2]
+
+mongo.connect(url, function(err, db) {
+    if (err) throw err;
+    var collection = db.collection(process.argv[3])
+
+    var id = { _id : process.argv[4] }
+    
+    collection.remove(id, function (err, data){
+        if (err) throw err;
+        db.close();})
+    db.close();
+})
