987312b51b3c852c7250373ad58d98151fac332a
Tim Meijome <tim.meijome@gmail.com>
2016-04-09 09:36:41 -0600

    update

--- program_3.js
+++ program_3.js
@@ -1,25 +1,16 @@
-var mongo = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/learnyoumongo';
-
-var findParrots = function(db, callback) {
-    var cursor = db.collection('parrots').find({
-        age: {$gt: parseInt(process.argv[2])}
-    }, {
-      name: 1
-    , age: 1
-    , _id: 0
-    });
-   
-    cursor.toArray(function(err, doc) {
-        if (err) throw err
-        console.log(doc);
-        db.close();
-    });
-};
+var mongo = require('mongodb').MongoClient
+var url = 'mongodb://localhost:27017/learnyoumongo'
 
 mongo.connect(url, function(err, db) {
-    if (err) throw err
-    findParrots(db, function() {
-        db.close();
-    });
-});
+    if (err) throw err;
+    var collection = db.collection('docs')
+    var firstlast = {
+        'firstName' : process.argv[2],
+        'lastName' : process.argv[3]
+    }
+    console.log(JSON.stringify(firstlast))
+    collection.insert(firstlast, function (err, data){
+        if (err) throw err;
+        db.close();})
+    db.close();
+})
